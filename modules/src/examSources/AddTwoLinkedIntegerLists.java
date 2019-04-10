13d5d986270fe0ad0b655054ab3447122a578c9d
Alexander <langadakis+bitbucket@gmail.com>
2019-04-09 22:10:32 -0400

    add two linked integer lists completed

--- modules/src/examSources/AddTwoLinkedIntegerLists.java
+++ modules/src/examSources/AddTwoLinkedIntegerLists.java
@@ -26,14 +26,48 @@ public class AddTwoLinkedIntegerLists
 
 	public static ListNode addTwoNumbers(ListNode l1, ListNode l2)
 	{
-		ListNode beginning = null;
-		ListNode current = beginning;
+		ListNode beginning = new ListNode(0);
+		//Get the first digit in. Guaranteed to be at least 1 digit long even if 0.
 		Pair<Integer,Integer> onesDigitAndOverflow = CommonHelperMethods.addTwoSingleDigitIntegers(l1.val,l2.val);
+
 		Integer onesValue = onesDigitAndOverflow.getKey();
+		beginning.val = onesValue;
+		ListNode current = beginning;
 		Integer overflowValue = onesDigitAndOverflow.getValue();
 
+		l1 = l1.next;
+		l2 = l2.next;
+		//while both ListNodes not null, AND overflow value not 0, do stuffs. And overflow for handling 90 + 10.
+		while(l1 != null || l2 != null || overflowValue > 0)
+		{
+			int l1Value = 0;
+			int l2Value = 0;
+			//set values if there are. Otherwise, assume as 0, and dont go down the list further
+			if(l1 != null)
+			{
+				l1Value = l1.val;
+				l1 = l1.next;
+			}
+			else l1Value = 0;
+			if(l2 != null)
+			{
+				l2Value = l2.val;
+				l2 = l2.next;
+			}
+			else l2Value = 0;
 
-
-		return null;
+			//add the two values
+			onesDigitAndOverflow = CommonHelperMethods.addTwoSingleDigitIntegers(l1Value, l2Value);
+			onesValue = onesDigitAndOverflow.getKey();
+			int tempOverflow = onesDigitAndOverflow.getValue();
+			//Add the resulting two values + previous overflow. (A 99 + 01 situation gives 9+1 the second time around)
+			onesDigitAndOverflow = CommonHelperMethods.addTwoSingleDigitIntegers(onesValue, overflowValue);
+			//Mark down the ones and carry on the overflow.
+			current.next = new ListNode(onesDigitAndOverflow.getKey());
+			current = current.next;
+			//a 99 + 01 situation should yield a 1 0 0, so the 2nd time it has to overflow
+			overflowValue = tempOverflow + onesDigitAndOverflow.getValue();
+		}
+		return beginning;
 	}
 }
--- modules/src/test/AddTwoLinkedIntegerListsTest.java
+++ modules/src/test/AddTwoLinkedIntegerListsTest.java
@@ -26,4 +26,21 @@ class AddTwoLinkedIntegerListsTest
 		assert resultNode.next.val == 0;
 		assert resultNode.next.next.val == 8;
 	}
+
+	@Test
+	void leetcodeExamplePositiveEdgeCase99And1()
+	{
+		//Input: (9 -> 9) + (1)
+		//Output: 0 -> 0 -> 1
+		//Explanation: 99 + 1 = 100
+
+		AddTwoLinkedIntegerLists.ListNode listNode1 = new AddTwoLinkedIntegerLists.ListNode(9);
+		listNode1.next = new AddTwoLinkedIntegerLists.ListNode(9);
+		AddTwoLinkedIntegerLists.ListNode listNode2 = new AddTwoLinkedIntegerLists.ListNode(1);
+
+		AddTwoLinkedIntegerLists.ListNode resultNode = AddTwoLinkedIntegerLists.addTwoNumbers(listNode1, listNode2);
+		assert resultNode.val == 0;
+		assert resultNode.next.val == 0;
+		assert resultNode.next.next.val == 1;
+	}
 }
